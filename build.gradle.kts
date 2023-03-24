c0b1aa341f7ee86c150fe8b6c38d5c5a59ff196c
xef5000 <64156533+xef5000@users.noreply.github.com>
2023-03-24 16:48:29 -0400

    little modification

--- build.gradle.kts
+++ build.gradle.kts
@@ -59,6 +59,8 @@ val shadowImpl: Configuration by configurations.creating {
 }
 
 dependencies {
+    implementation("org.jetbrains:annotations:23.0.0")
+    implementation("org.jetbrains:annotations:23.0.0")
     minecraft("com.mojang:minecraft:1.8.9")
     mappings("de.oceanlabs.mcp:mcp_stable:22-1.8.9")
     forge("net.minecraftforge:forge:1.8.9-11.15.1.2318-1.8.9")
