f3c0403fbb9c6baa82972f09eba3f3de496994bb
xef5000 <64156533+xef5000@users.noreply.github.com>
2023-04-01 20:56:51 -0400

    New core class used to manage all features

---
+++ src/main/java/com/xef5000/Feature.java
@@ -0,0 +1,57 @@
+package com.xef5000;
+
+import com.xef5000.gui.buttons.ButtonLocation;
+import com.xef5000.listeners.RenderListener;
+import net.minecraft.client.Minecraft;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+public enum Feature {
+
+    TEST_TEXT(1, 0, "testText");
+
+
+    public static final Set<Feature> guiFeatures = new HashSet<>(Arrays.asList(new Feature[]{TEST_TEXT}));
+
+
+
+    private int id;
+
+    // Drawtypes:
+    // 0 = text
+    private int drawType;
+    private String configName;
+
+    Feature(int id, int drawType, String configName) {
+        this.id = id;
+        this.drawType = drawType;
+        this.configName = configName;
+        FrogMod.INSTANCE.getRegisteredFeatureIDs().add(id);
+    }
+
+
+    public void draw(float scale, Minecraft mc, ButtonLocation buttonLocation) {
+        if (drawType == 0) {
+            RenderListener.getInstance().drawText(this, scale, mc, buttonLocation);
+        }
+
+    }
+
+    public String getConfigName() {return configName;}
+    public int getDrawType() {return drawType;}
+
+    public static Set<Feature> getGuiFeatures() { return guiFeatures; }
+    public int getId() {return id;}
+
+    public static Feature fromId(int id) {
+        for (Feature feature : values()) {
+            if (feature.getId() == id) {
+                return feature;
+            }
+        }
+        return null;
+    }
+
+}
